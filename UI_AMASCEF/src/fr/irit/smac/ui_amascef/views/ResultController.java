510bdfd0722aa26edf76b0fcdefafe3b47b2231f
MarcillaudG <guilhem.marcillaud@gmail.com>
2019-05-07 15:18:41 +0200

    rootitem selected

--- UI_AMASCEF/src/fr/irit/smac/ui_amascef/views/ResultController.java
+++ UI_AMASCEF/src/fr/irit/smac/ui_amascef/views/ResultController.java
@@ -23,71 +23,76 @@ import javafx.scene.input.MouseEvent;
 
 
 public class ResultController{
-	
-	
+
+
 	@FXML 
 	private TreeView<String> tree;
-	
+
 	@FXML
 	private TableView<AGFModel> table;
-	
+
 	@FXML 
 	private TableColumn<AGFModel, Integer> parametersMissing;
-	
+
 	@FXML 
 	private TableColumn<AGFModel, Integer> parametersSent;
-	
+
 	@FXML
 	private TableColumn<AGFModel, String> names;
-	
+
 	private AMASCEFModel model;
 
 	private ResultMain mainApp;
-	
+
 	private ObservableList<AGFModel> agfData = FXCollections.observableArrayList();
-	
+
 	private Map<String,AGFModel> agfNotObserved;
-	
+
 	public ResultController() {
 		this.model = new AMASCEFModel();
 	}
 
-	
+
 	@FXML
 	private void initialize() {
-		
+
 		this.agfNotObserved = new TreeMap<String,AGFModel>();
 		// Initialize the tree
 		// Root Item
-	    TreeItem<String> rootItem = new TreeItem<String>("AGFunction");
-	    rootItem.setExpanded(true);
-		
-	   /* // Item 1
+		CheckBoxTreeItem<String> rootItem = new CheckBoxTreeItem<String>("AGFunction");
+		rootItem.setExpanded(true);
+		rootItem.setSelected(true);
+		/* // Item 1
         CheckBoxTreeItem<String> item1 = new CheckBoxTreeItem<String>("Test11");
- 
+
         // Item 2
         CheckBoxTreeItem<String> item2 = new CheckBoxTreeItem<String>("Test12");
- 
+
         // Item 3
         CheckBoxTreeItem<String> item3 = new CheckBoxTreeItem<String>("Test13");*/
-        
-        this.tree.setRoot(rootItem);
-        tree.setCellFactory( CheckBoxTreeCell.<String> forTreeView());
-        
-        
-		
-		 this.tree.setVisible(true);
-		 
-		 names.setCellValueFactory(cellData -> cellData.getValue().getName());	 
-		 parametersSent.setCellValueFactory(cellData -> cellData.getValue().getNbParamsSent().asObject());	 
-		 parametersMissing.setCellValueFactory(cellData -> cellData.getValue().getNbParamsReceive().asObject());
-		
+
+		rootItem.selectedProperty().addListener((obs, oldVal, newVal) -> {
+			for(TreeItem<String> child : rootItem.getChildren()) {
+				((CheckBoxTreeItem<String>)child).setSelected(rootItem.isSelected());
+			}
+		});
+		this.tree.setRoot(rootItem);
+		tree.setCellFactory( CheckBoxTreeCell.<String> forTreeView());
+
+
+
+		this.tree.setVisible(true);
+
+		names.setCellValueFactory(cellData -> cellData.getValue().getName());	 
+		parametersSent.setCellValueFactory(cellData -> cellData.getValue().getNbParamsSent().asObject());	 
+		parametersMissing.setCellValueFactory(cellData -> cellData.getValue().getNbParamsReceive().asObject());
+
 	}
 
 
 	public void setMainApp(ResultMain resultMain) {
 		this.mainApp = resultMain;
-		
+
 	}
 
 
@@ -97,47 +102,50 @@ public class ResultController{
 			AGFModel tmp = new AGFModel(agf.getName());
 			this.model.addListenerToAGFunction(tmp,agf.getName());
 			this.agfData.add(tmp);
-			
+
 		}
-		
+
 		initializeTree();
 		initializeTable();
 	}
 
 
 	private void initializeTable() {
-		
+
 		this.table.setItems(this.agfData);
-		
+
 	}
 
 
 	private void initializeTree() {
 		for(AGFunction agf : this.model.getAmas().getAllAGF()) {
-	        CheckBoxTreeItem<String> item = new CheckBoxTreeItem<String>(agf.getName());
-	        item.setSelected(true);
-	        item.selectedProperty().addListener((obs, oldVal, newVal) -> {
-	            if(this.agfNotObserved.containsKey(item.getValue())) {
-	            	this.agfData.add(this.agfNotObserved.remove(item.getValue()));
-	            }
-	            else {
-	            	int indAGF = -1;
-	            	for(int i = 0;i < this.agfData.size();i++) {
-	            		if(this.agfData.get(i).getName().get().equals(item.getValue())) {
-	            			indAGF = i;
-	            		}
-	            	}
-	            	this.agfNotObserved.put(item.getValue(), this.agfData.remove(indAGF));
-	            }
-	        });
-	        this.tree.getRoot().getChildren().add(item);
+			CheckBoxTreeItem<String> item = new CheckBoxTreeItem<String>(agf.getName());
+			item.setSelected(true);
+			item.selectedProperty().addListener((obs, oldVal, newVal) -> {
+				if(item.isSelected()) {
+					if(this.agfNotObserved.containsKey(item.getValue())) {
+						this.agfData.add(this.agfNotObserved.remove(item.getValue()));
+					}
+				}
+				else {
+					int ind = -1;
+					for(int i = 0; i < this.agfData.size();i++) {
+						if(this.agfData.get(i).getName().get().equals(item.getValue())) {
+							ind = i;
+						}
+					}
+					if(ind != -1)
+						this.agfNotObserved.put(item.getValue(), this.agfData.remove(ind));
+				}
+			});
+			this.tree.getRoot().getChildren().add(item);
 		}
 	}
 
 	@FXML
 	private void handleClickOnTree() {
-		
+
 	}
-	
-	
+
+
 }
